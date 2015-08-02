0d2b5e0738b04620184b22bf99ab958d833114cb
marcopacheco <marcopache@gmail.com>
2015-08-02 11:00:19 -0600

    First commit
    First stage of project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LonelyPlanetTest::Application.config.secret_key_base = '88bfc2c68dff23b42c4056ac666b3291373ec1573b4aea090b9bdc939ac85670f2cf2423e90b3c7265bd1ead44d1c6c4f12ed5b59b1b5df2fb586180f2665e56'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LonelyPlanetTest::Application.config.session_store :cookie_store, key: '_lonely_planet_test_session'
